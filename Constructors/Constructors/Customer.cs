a6e806401b34cda2c5bb37cbea1bd40e1d249917
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-03 23:03:30 -0500

    overloading and using this

--- Constructors/Constructors/Customer.cs
+++ Constructors/Constructors/Customer.cs
@@ -10,19 +10,22 @@ namespace Constructors
     {
         public int Id;
         public string Name;
+        public List<Order> Orders;
 
         public Customer()
         {
+            Orders = new List<Order>();
         }
 
         public Customer(int id)
+            : this()
         {
             this.Id = id;
         }
 
         public Customer(int id, string name)
+            : this(id)
         {
-            this.Id = id;
             this.Name = name;
         }
     }
--- Constructors/Constructors/Program.cs
+++ Constructors/Constructors/Program.cs
@@ -11,6 +11,10 @@ namespace Constructors
         static void Main(string[] args)
         {
             var customer = new Customer(1, "John");
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
             Console.WriteLine(customer.Id);
             Console.WriteLine(customer.Name);
         }
