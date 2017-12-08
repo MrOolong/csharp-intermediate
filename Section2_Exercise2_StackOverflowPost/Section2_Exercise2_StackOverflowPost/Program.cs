260ae5f727903b8cb10b7316eb7410145d180c16
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-07 19:34:31 -0500

    Section 2 - Exercise 2 - Initial commit

---
+++ Section2_Exercise2_StackOverflowPost/Section2_Exercise2_StackOverflowPost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section2_Exercise2_StackOverflowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Post
+    {
+
+    }
+}
