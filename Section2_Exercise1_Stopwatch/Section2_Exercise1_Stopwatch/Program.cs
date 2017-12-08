1e3ebc9975700351ed5c68f7b9933508c455a77b
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-07 19:29:43 -0500

    Basic start

--- Section2_Exercise1_Stopwatch/Section2_Exercise1_Stopwatch/Program.cs
+++ Section2_Exercise1_Stopwatch/Section2_Exercise1_Stopwatch/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Section2_Exercise1_Stopwatch
 {
@@ -10,6 +11,23 @@ namespace Section2_Exercise1_Stopwatch
     {
         static void Main(string[] args)
         {
+            // prompt user to start the timer
+            
+            // prompt user to stop the timer
+        }
+    }
+
+    public class Stopwatch
+    {
+        public void Start()
+        {
+            DateTime start = new DateTime();
+            DateTime stop = new DateTime();
+        }
+
+        public void Stop()
+        {
+
         }
     }
 }
